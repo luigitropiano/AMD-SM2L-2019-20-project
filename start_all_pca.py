d7b22d975379bcd04aa88d2dda1339bf851bc93f
Luigi Tropiano <luigi.tropiano@studenti.unimi.it>
2021-05-17 00:32:21 +0200

    FIX start_all_pca.py

--- start_all_pca.py
+++ start_all_pca.py
@@ -23,7 +23,7 @@ df = amc.load_dataset(DATA_PATH, spark)
 ###############################################################
 ## PREPROCESSING: FEATURES ENGINEERING
 
-# name of the target column
+# name of the target column and remove all the rows where 'PINCP' is null
 target = 'PINCP'
 df = df.dropna(subset = target)
 
@@ -31,55 +31,50 @@ df = df.dropna(subset = target)
 skipping = ['PERNP', 'WAGP', 'HINCP', 'FINCP']
 numericals = ['NP', 'BDSP', 'CONP', 'ELEP', 'FULP', 'INSP', 'MHP', 'MRGP', 'RMSP', 'RNTP', 'SMP', 'VALP', 'WATP', 'GRNTP', 'GRPIP', 'GASP', 'NOC', 'NPF', 'NRC', 'OCPIP', 'SMOCP', 'AGEP', 'INTP', 'JWMNP', 'OIP', 'PAP', 'RETP', 'SEMP', 'SSIP', 'SSP', 'WKHP', 'POVPIP']
 ordinals = ['AGS', 'YBL', 'MV', 'TAXP', 'CITWP', 'DRAT', 'JWRIP', 'MARHT', 'MARHYP', 'SCHG', 'SCHL', 'WKW', 'YOEP', 'DECADE', 'JWAP', 'JWDP', 'SFN']
-categoricals = [col for col in df.columns if col not in skipping + numericals + ordinals]
+categoricals = [col for col in df.columns if col not in skipping + numericals + ordinals + [target]]
 
 ################################################################
 #fill all null numericals value with 0
 df = df.fillna(0, numericals)
 
 ###############################################################
-
-#ENCODING and INDEXING categorical features
-
+#INDEXING AND ENCODING
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import PCA
 from pyspark.ml.feature import OneHotEncoder
 from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml.feature import StandardScaler
+from pyspark.sql.functions import rand
 
 utils.printNowToFile("starting StringIndexer + OneHotEncoder pipeline:")
 
-indexers = [StringIndexer(inputCol=col, outputCol=col+"_index", handleInvalid='keep') for col in ordinals + categoricals]
-encoders = [OneHotEncoder(inputCol=col+"_index", outputCol=col+"_encode", dropLast = True) for col in categoricals]
-
-#list of features indexed and encoded
 ordinals_input = [col+"_index" for col in ordinals]
 categoricals_input = [col+"_encode" for col in categoricals]
 
-
-#vector assembler pipeline to create numerical, ordinals and categoricals vector input for scaler
-vector = [
+indexers = [StringIndexer(inputCol=col, outputCol=col+"_index", handleInvalid='keep') for col in ordinals + categoricals]
+encoders = [OneHotEncoder(inputCol=col+"_index", outputCol=col+"_encode", dropLast = True) for col in categoricals]
+assemblers = [
     VectorAssembler(inputCols = numericals, outputCol = 'numericals_vector', handleInvalid='keep'),
     VectorAssembler(inputCols = ordinals_input, outputCol = 'ordinals_vector'),
     VectorAssembler(inputCols = categoricals_input, outputCol = 'categoricals_vector')
 ]
 
-df = Pipeline(stages = indexers + encoders + vector).fit(df).transform(df)
+df = Pipeline(stages = indexers + encoders + assemblers).fit(df).transform(df)
 
 #Drop useless features
 utils.printNowToFile("dropping useless columns:")
-useless_col = ordinals_input + categoricals_input + numericals
+useless_col = numericals + ordinals_input + categoricals_input
 
 df = df.drop(*useless_col)
 
 # SPLIT DATASET
-df = df.persist(StorageLevel.MEMORY_AND_DISK)
-( train_set, val_set, test_set ) = df.randomSplit([0.6, 0.2, 0.2])
+#df = df.persist(StorageLevel.MEMORY_AND_DISK)
+( train_set, test_set ) = df.orderBy(rand()).randomSplit([0.7, 0.3])
 
 ###############################################################
-#STD SCALER 
+#SCALING
 
-utils.printNowToFile("starting VectorAssembler + StandardScaler pipeline:")
+utils.printNowToFile("starting scalers pipelines:")
 
 scaledFeatures = ['numericals_scaled', 'ordinals_scaled', 'categoricals_scaled']
 
@@ -94,9 +89,15 @@ stages = [
 pipeline = Pipeline(stages = stages).fit(train_set)
 train_set = pipeline.transform(train_set)
 test_set = pipeline.transform(test_set)
-val_set = pipeline.transform(val_set)
+utils.printNowToFile("end pipeline std")
 
-#####################################################################
+#Drop useless features
+utils.printNowToFile("dropping useless columns:")
+useless_col = ['numericals_vector', 'ordinals_vector', 'categoricals_vector']
+train_set = train_set.drop(*useless_col)
+test_set = test_set.drop(*useless_col)
+
+################################################################
 #TUNING WITH K-FOLD CROSS VALIDATION
 utils.printNowToFile("starting CrossValidation:")
 
@@ -107,65 +108,60 @@ import numpy as np
 def unionAll(*dfs):
     return reduce(DataFrame.unionByName, dfs)
 
-#split train set in 5 fold
-fold_1, fold_2, fold_3, fold_4, fold_5 = train_set.randomSplit([0.2, 0.2, 0.2, 0.2, 0.2])
+features_columns = ['features_std']
+for features_column in features_columns:
 
-scores_dict = {}
-folds = [fold_1, fold_2, fold_3, fold_4, fold_4]
+    utils.printNowToFile("starting CrossValidation for " + features_column + ":")
 
-for alpha in [0.01, 0.1, 1]:
-    utils.printNowToFile('trying alpha = ' + str(alpha))
-    partial_scores = np.array([])
-    srrcv = rr.SparkRidgeRegression(reg_factor=alpha)
-    for fold in folds:
-        folds_to_merge = [f for f in folds if f != fold]
-        cv = unionAll(*folds_to_merge)
-        cv = cv.coalesce(100)
-        srrcv.fit(cv)
-        result = srrcv.predict_many(test_set)
-        partial_scores = np.append(partial_scores, srrcv.r2(result.select('PINCP', 'target_predictions')))
-    final_score = np.mean(partial_scores)
-    scores_dict[alpha] = final_score
+    fold_1, fold_2, fold_3, fold_4, fold_5 = train_set.randomSplit([0.2, 0.2, 0.2, 0.2, 0.2])
 
-for k in scores_dict:
-    utils.printNowToFile('alpha ' + str(k) + ' - r2 score ' + str(scores_dict[k]))
+    scores_dict = {}
+    folds = [fold_1, fold_2, fold_3, fold_4, fold_5]
 
-#find best value for alpha
-best_alpha = max(scores_dict, key=scores_dict.get)
-utils.printNowToFile('selected alpha: ' + str(best_alpha))
+    for alpha in [0.01, 0.1, 1]:
+        utils.printNowToFile('trying alpha = ' + str(alpha))
+        partial_scores = np.array([])
+        srrcv = rr.SparkRidgeRegression(reg_factor=alpha)
+        for fold in folds:
+            folds_to_merge = [f for f in folds if f != fold]
+            cv = unionAll(*folds_to_merge)
+            cv = cv.coalesce(100)
+            srrcv.fit(cv, features_column)
+            result = srrcv.predict_many(test_set, features_column, 'new_column')
+            partial_scores = np.append(partial_scores, srrcv.r2(result.select('PINCP', 'new_column')))
+        final_score = np.mean(partial_scores)
+        scores_dict[alpha] = final_score
 
+    for k in scores_dict:
+        utils.printNowToFile('alpha ' + str(k) + ' - r2 score ' + str(scores_dict[k]))
+
+    best_alpha = max(scores_dict, key=scores_dict.get)
+    utils.printNowToFile('selected alpha: ' + str(best_alpha))
 
 ################################################################
-#PREDICTION
-utils.printNowToFile("starting SparkRidgeRegression:")
 
-#persist train_set on disk memory
-train_set = train_set.persist(StorageLevel.DISK_ONLY)
+    utils.printNowToFile("starting SparkRidgeRegression:")
 
-#Fit ridge regression model on train set
-srr = rr.SparkRidgeRegression(reg_factor=best_alpha)
-#train_set = train_set.withColumn('features_final', train_set.scaledFeatures)
-#test_set = test_set.withColumn('features_final', train_set.scaledFeatures)
-utils.printNowToFile("pre srr fit:")
-srr.fit(train_set)
-utils.printNowToFile("post srr fit:")
+    train_set = train_set.persist(StorageLevel.DISK_ONLY)
 
-#r2 score on test set
-result = srr.predict_many(test_set)
-utils.printToFile('result: {0}'.format(srr.r2(result.select('PINCP', 'target_predictions'))))
+    utils.printNowToFile("pre srr fit:")
+    srr = rr.SparkRidgeRegression(reg_factor=best_alpha)
+    srr.fit(train_set, features_column)
+    utils.printNowToFile("post srr fit:")
 
-#fit model with pyspark ridge regression
-lin_reg = LinearRegression(standardization = False, featuresCol = 'features_final', labelCol='PINCP', maxIter=10, regParam=best_alpha, elasticNetParam=0.0, fitIntercept=True)
-utils.printNowToFile("starting linear transform:")
-linear_mod = lin_reg.fit(train_set)
-utils.printNowToFile("after linear transform:")
+    result = srr.predict_many(test_set, features_column, 'new_column')
+    utils.printToFile('result: {0}'.format(srr.r2(result.select('PINCP', 'new_column'))))
 
-#r2 score on test set
-predictions = linear_mod.transform(test_set)
-y_true = predictions.select("PINCP").toPandas()
-y_pred = predictions.select("prediction").toPandas()
-r2_score = sklearn.metrics.r2_score(y_true, y_pred)
-utils.printToFile('r2_score before: {0}'.format(r2_score))
+    utils.printNowToFile("starting linear transform:")
+    lin_reg = LinearRegression(standardization = False, featuresCol = features_column, labelCol='PINCP', maxIter=10, regParam=best_alpha, elasticNetParam=0.0, fitIntercept=True)
+    linear_mod = lin_reg.fit(train_set)
+    utils.printNowToFile("after linear transform:")
+
+    predictions = linear_mod.transform(test_set)
+    y_true = predictions.select("PINCP").toPandas()
+    y_pred = predictions.select("prediction").toPandas()
+    r2_score = sklearn.metrics.r2_score(y_true, y_pred)
+    utils.printToFile('r2_score before: {0}'.format(r2_score))
 
 '''
 predict_target = ['PINCP', 'new_column']
