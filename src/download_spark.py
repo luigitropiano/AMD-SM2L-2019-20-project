557c153582c6a90ded3491fbbb422db606bcc881
Luigi Tropiano <luigi.tropiano@studenti.unimi.it>
2021-05-31 17:45:09 +0200

    FIX download_spark.download()

--- src/download_spark.py
+++ src/download_spark.py
@@ -3,12 +3,13 @@ import urllib.request
 from src import utils
 
 def download(PATH):
-    if not os.path.exists(PATH + '/spark'):
-        os.makedirs(PATH + '/spark')
-    if not os.path.exists(PATH + '/spark/spark_tmp'):
-        os.makedirs(PATH + '/spark/spark_tmp')
-    if not os.path.exists(PATH + '/spark/spark/spark-3.1.1-bin-hadoop3.2'):
+    SPARK = PATH + '/spark'
+    if not os.path.exists(SPARK):
+        os.makedirs(SPARK)
+    if not os.path.exists(SPARK + '/spark_tmp'):
+        os.makedirs(SPARK + '/spark_tmp')
+    if not os.path.exists(SPARK + '/spark-3.1.1-bin-hadoop3.2'):
         thetarfile = "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz"
         ftpstream = urllib.request.urlopen(thetarfile)
         thetarfile = tarfile.open(fileobj=ftpstream, mode="r|gz")
-        thetarfile.extractall()
+        thetarfile.extractall(path = SPARK)
